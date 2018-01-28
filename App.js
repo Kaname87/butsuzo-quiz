82e2bb9f63c8ea1af3ffb18ddce05a00784194ff
Kaname87 <kaname.nakanishi@bibo.com.ph>
2018-01-28 22:31:34 +0800

    Implement quiz iteration

--- App.js
+++ App.js
@@ -12,39 +12,84 @@ import Quiz from './Quiz';
 
 import data from './data.json'
 
+const maxQuestion = 3;
+
 export default class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       number: 0,
       correctNumber: 0,
+      showResult: false,
+      isCorrect: false,
+      quizSet: this.getNewQuizSet(0),
+      isFinished: false,
     }
-    
+
     this._onPressButton = this._onPressButton.bind(this);
   }
-  
-  _onPressButton(id){
-    Alert.alert(`ID is ${id}`);
+
+  getNewQuizSet(idx) {
+    return data.quizSet[idx] || data.quizSet[0];
   }
-        
+
+  _checkAnswer(id) {
     
+    return this.state.quizSet.answer === id;
+  }
+
+  _onPressButton(id){
+    const isCorrect = this._checkAnswer(id);
+
+    this.setState(prevState => {
+      if (prevState.number === this.state.number) {
+        const newState = {
+          number: prevState.number + 1,
+          showResult: true,
+          isFinished: prevState.number === maxQuestion,
+        }
+
+        if (isCorrect) {
+          return {
+            ...newState,
+            correctNumber: prevState.correctNumber + 1,
+            isCorrect: true,
+          }
+        } else {
+          return newState;
+        }
+      }
+    })
+
+    // 若干遅くよみとる
+    setTimeout(() => {
+      this.setState(prevState =>({
+        quizSet: this.getNewQuizSet(prevState.number),
+        showResult: false,
+      }))}, 1000
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.progressBar}>
           <Text>Prpgress Bar</Text>
+          <Text>{this.state.number}/{maxQuestion}</Text>
         </View>
         <Quiz 
-          name={data.quizSet.name}
-          place={data.quizSet.place}
+          name={this.state.quizSet.name}
+          place={this.state.quizSet.place}
           number={this.state.number}
         />
-        <View style={styles.progressBar}>
-          <Text>Result</Text>
-        </View>
+        {this.state.showResult &&
+          <View style={styles.progressBar}>
+            <Text>{this.state.isCorrect ? "Correct" : "Wrong"} </Text>
+          </View>
+        }
         <AnswerOptionsContainer 
-          anserOptions={data.quizSet.anserOptions}
+          anserOptions={this.state.quizSet.anserOptions}
           onPressButton={this._onPressButton}
         />
       </View>
